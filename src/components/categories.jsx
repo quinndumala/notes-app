5662d0aa17399d395bd132c7e282b8788e9f7883
Quinn Dumala <quinndumala@gmail.com>
2022-06-19 23:21:01 +0800

    init

---
+++ src/components/categories.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+
+export default function Categories({ categoryProps }) {
+  const { category, handleSetCategory } = categoryProps;
+
+  return (
+    <>
+      <FormLabel disabled>Note Category</FormLabel>
+      <RadioGroup value={category}>
+        <FormControlLabel
+          onChange={(e) => handleSetCategory(e.target.value)}
+          value="todos"
+          control={<Radio />}
+          label="Todos"
+        />
+        <FormControlLabel
+          onChange={(e) => handleSetCategory(e.target.value)}
+          value="reminders"
+          control={<Radio />}
+          label="Reminders"
+        />
+        <FormControlLabel
+          onChange={(e) => handleSetCategory(e.target.value)}
+          value="personal"
+          control={<Radio />}
+          label="Personal"
+        />
+        <FormControlLabel
+          onChange={(e) => handleSetCategory(e.target.value)}
+          value="work"
+          control={<Radio />}
+          label="Work"
+        />
+      </RadioGroup>
+    </>
+  );
+}
+
+Categories.propTypes = {
+  categoryProps: PropTypes.shape({
+    category: PropTypes.string,
+    handleSetCategory: PropTypes.func
+  })
+};
---
+++ src/pages/Create.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  Typography,
+  Button,
+  Container,
+  TextField,
+  FormControl
+} from "@mui/material";
+import { Save } from "@mui/icons-material";
+import Categories from "../components/categories";
+
+// const useStyles = makeStyles({
+//   field: {
+//     marginTop: 20,
+//     marginBottom: 20,
+//     display: "block"
+//   }
+// });
+const styles = {
+  field: {
+    marginTop: "20px",
+    marginBottom: "20px",
+    display: "block"
+  }
+};
+
+export default function Create() {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const clearTextFieldErrors = () => {
+    setTitleError(false);
+    setDetailsError(false);
+  };
+
+  const handleSetCategory = (value) => {
+    setCategory(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearTextFieldErrors();
+
+    if (title === "") setTitleError(true);
+    if (details === "") setDetailsError(true);
+
+    if (title && details) {
+      console.log("title, details, category", title, details, category);
+    }
+  };
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit} autoComplete="off" noValidate>
+        <Typography
+          variant="h6"
+          color="textSecondary"
+          component="h2"
+          gutterBottom>
+          Create a New Note
+        </Typography>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          id="noteTitle"
+          sx={styles.field}
+          variant="outlined"
+          color="secondary"
+          label="Note Title"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          id="noteDetails"
+          sx={styles.field}
+          variant="outlined"
+          color="secondary"
+          label="Details"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+        <FormControl sx={styles.field}>
+          <Categories categoryProps={{ category, handleSetCategory }} />
+        </FormControl>
+
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          startIcon={<Save />}
+          disableElevation>
+          SAVE
+        </Button>
+      </form>
+    </Container>
+  );
+}
