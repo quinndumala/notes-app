adb4c46cbc98303e3141f257d9821cd3c3ac8cf5
Quinn Dumala <quinndumala@gmail.com>
2022-09-11 23:42:44 +0800

    complete notes component

--- views/src/components/notes.jsx
+++ views/src/components/notes.jsx
@@ -24,6 +24,8 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { authMiddleWare } from "../util/auth";
 import { useHistory } from "react-router-dom";
+import { createRef } from "react";
+import { useEffect } from "react";
 
 const styles = (theme) => ({
   content: {
@@ -88,23 +90,24 @@ const styles = (theme) => ({
   }
 });
 
-const Transition = React.forwardRef(function Transition(props, ref) {
+const Transition = React.forwardRef((props, ref) => {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 function Notes({ classes }) {
-  const [notes, setNotes] = useState("");
+  const [notes, setNotes] = useState([]);
   const [noteDetails, setNoteDetails] = useState({
     title: "",
     body: ""
   });
-  const [noteID, setNoteId] = useState("");
+  const [noteId, setNoteId] = useState("");
   const [errors, setErrors] = useState([]);
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [uiLoading, setUiLoading] = useState(true);
   const [buttonType, setButtonType] = useState("");
   const [viewOpen, setViewOpen] = useState(false);
 
+  const dialogRef = createRef(null);
   const history = useHistory();
 
   useState(() => {
@@ -121,6 +124,13 @@ function Notes({ classes }) {
         console.log(err);
       });
   }, []);
+
+  const handleTextChange = (event) => {
+    setNoteDetails({
+      ...noteDetails,
+      [event.target.name]: event.target.value
+    });
+  };
 
   const handleDeleteNote = (data) => {
     authMiddleWare(history);
@@ -144,7 +154,7 @@ function Notes({ classes }) {
     });
     setNoteId(data.note.noteId);
     setButtonType("Edit");
-    setOpen(true);
+    setIsOpen(true);
   };
 
   const handleViewOpen = (data) => {
@@ -187,13 +197,216 @@ function Notes({ classes }) {
       body: ""
     });
     setButtonType("");
-    setOpen(true);
+    setIsOpen(true);
   };
+
+  const handleSubmit = (event) => {
+    authMiddleWare(history);
+    event.preventDefault();
+    const userNote = {
+      title: noteDetails.title,
+      body: noteDetails.body
+    };
+    let options = {};
+    if (buttonType === "Edit") {
+      options = {
+        url: `/note/${noteId}`,
+        method: "put",
+        data: userNote
+      };
+    } else {
+      options = {
+        url: "/addNote",
+        method: "post",
+        data: userNote
+      };
+    }
+    const authToken = localStorage.getItem("AuthToken");
+    axios.defaults.headers.common = { Authorization: `${authToken}` };
+    axios(options)
+      .then(() => {
+        setIsOpen(false);
+        window.location.reload();
+      })
+      .catch((error) => {
+        setIsOpen(true);
+        setErrors(error.response.data);
+        console.log(error);
+      });
+  };
+
+  const handleViewClose = () => {
+    setViewOpen(false);
+  };
+
+  const handleClose = (event) => {
+    setIsOpen(false);
+  };
+
+  if (uiLoading === true) {
+    return (
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {uiLoading && (
+          <CircularProgress size={150} className={classes.uiProgess} />
+        )}
+      </main>
+    );
+  } else {
+    return (
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+
+        <IconButton
+          className={classes.floatingButton}
+          color="primary"
+          aria-label="Add Todo"
+          onClick={handleClickOpen}>
+          <AddCircleIcon style={{ fontSize: 60 }} />
+        </IconButton>
+        <Dialog
+          ref={dialogRef}
+          fullScreen
+          open={isOpen}
+          onClose={handleClose}
+          TransitionComponent={Transition}>
+          <AppBar className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleClose}
+                aria-label="close">
+                <CloseIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                {buttonType === "Edit" ? "Edit Todo" : "Create a new Note"}
+              </Typography>
+              <Button
+                autoFocus
+                color="inherit"
+                onClick={handleSubmit}
+                className={classes.submitButton}>
+                {buttonType === "Edit" ? "Save" : "Submit"}
+              </Button>
+            </Toolbar>
+          </AppBar>
+
+          <Toolbar />
+
+          <form className={classes.form} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="noteTitle"
+                  label="Note Title"
+                  name="title"
+                  autoComplete="noteTitle"
+                  helperText={errors.title}
+                  value={noteDetails.title}
+                  error={errors.title ? true : false}
+                  onChange={handleTextChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  id="noteDetails"
+                  label="Note Details"
+                  name="body"
+                  autoComplete="noteDetails"
+                  multiline
+                  minRows={25}
+                  maxRows={25}
+                  helperText={errors.body}
+                  error={errors.body ? true : false}
+                  onChange={handleTextChange}
+                  value={noteDetails.body}
+                />
+              </Grid>
+            </Grid>
+          </form>
+        </Dialog>
+
+        <Grid container spacing={2}>
+          {notes.map((note) => (
+            <Grid item xs={12} sm={6}>
+              <Card className={classes.root} variant="outlined">
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {note.title}
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    {dayjs(note.createdAt).fromNow()}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {`${note.body.substring(0, 65)}`}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => handleViewOpen({ note })}>
+                    {" "}
+                    View{" "}
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => handleEditClickOpen({ note })}>
+                    Edit
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    onClick={() => handleDeleteNote({ note })}>
+                    Delete
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+
+        <Dialog
+          onClose={handleViewClose}
+          aria-labelledby="customized-dialog-title"
+          open={viewOpen}
+          fullWidth
+          classes={{ paperFullWidth: classes.dialogeStyle }}>
+          <DialogTitle id="customized-dialog-title" onClose={handleViewClose}>
+            {noteDetails.title}
+          </DialogTitle>
+          <DialogContent dividers>
+            <TextField
+              fullWidth
+              id="todoDetails"
+              name="body"
+              multiline
+              readonly
+              rows={1}
+              rowsMax={25}
+              value={noteDetails.body}
+              InputProps={{
+                disableUnderline: true
+              }}
+            />
+          </DialogContent>
+        </Dialog>
+      </main>
+    );
+  }
 
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
-      <Typography paragraph>Hello I am todo</Typography>
+      <Typography paragraph>Hello I am notes view</Typography>
     </main>
   );
 }
